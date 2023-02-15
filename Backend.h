52befd64c581f078958b89a3a0c3d4b497d34f64
Abhay Chirania <a-chirania@ti.com>
2023-02-15 14:21:40 +0530

    Backend: Update gstreamer pipelines
    Signed-off-by: Abhay Chirania <a-chirania@ti.com>
    Signed-off-by: Sai Sree Kartheek Adivi <s-adivi@ti.com>

--- Backend.h
+++ Backend.h
@@ -6,10 +6,32 @@
 
 using namespace std;
 
-static string od_pipeline = "multifilesrc location=/opt/edgeai-test-data/videos/video_0000_h264.h264 loop=true caps=\"video/x-h264, width=1280, height=720\" ! \
-                                  h264parse ! v4l2h264dec ! video/x-raw,format=NV12 ! \
-                                  tiovxmosaic sink_0::startx=\"<320>\" sink_0::starty=\"<180>\" sink_0::widths=\"<1280>\" sink_0::heights=\"<720>\" ! \
-                                  video/x-raw, width=1920, height=1080 ! queue ! tiperfoverlay title=\"Object Detection Demo\" ! ";
+static string cl_pipeline = "   multifilesrc location=/opt/edgeai-test-data/videos/video_0002_h264.h264 loop=true caps=\"video/x-h264, width=1280, height=720\" ! h264parse ! v4l2h264dec ! video/x-raw, format=NV12 ! \
+                                tiovxmultiscaler name=split_01 \
+                                split_01. ! queue ! video/x-raw, width=454, height=256 ! tiovxdlcolorconvert out-pool-size=4 ! video/x-raw, format=RGB ! videobox left=115 right=115 top=16 bottom=16 ! tiovxdlpreproc data-type=3 channel-order=1 tensor-format=rgb out-pool-size=4 ! application/x-tensor-tiovx ! tidlinferer model=/opt/model_zoo/TFL-CL-0000-mobileNetV1-mlperf ! post_0.tensor \
+                                split_01. ! queue ! video/x-raw, width=1280, height=720 ! post_0.sink \
+                                tidlpostproc name=post_0 model=/opt/model_zoo/TFL-CL-0000-mobileNetV1-mlperf alpha=0.400000 viz-threshold=0.500000 top-N=5 ! queue ! mosaic_0. \
+                                tiovxmosaic target=1 name=mosaic_0 \
+                                sink_0::startx=\"<320>\"  sink_0::starty=\"<180>\"  sink_0::widths=\"<1280>\"   sink_0::heights=\"<720>\"  \
+                                ! video/x-raw,format=NV12, width=1920, height=1080 ! queue ! tiperfoverlay main-title=null title=\"Image Classification\" ! ";
+
+static string od_pipeline = "   multifilesrc location=/opt/edgeai-test-data/videos/video_0000_h264.h264 loop=true caps=\"video/x-h264, width=1280, height=720\" ! h264parse ! v4l2h264dec ! video/x-raw,format=NV12 ! \
+                                tiovxmultiscaler name=split_01 \
+                                split_01. ! queue ! video/x-raw, width=320, height=320 ! tiovxdlpreproc data-type=3 channel-order=1 tensor-format=rgb out-pool-size=4 ! application/x-tensor-tiovx ! tidlinferer model=/opt/model_zoo/TFL-OD-2020-ssdLite-mobDet-DSP-coco-320x320 ! post_0.tensor \
+                                split_01. ! queue ! video/x-raw, width=1280, height=720 ! post_0.sink \
+                                tidlpostproc name=post_0 model=/opt/model_zoo/TFL-OD-2020-ssdLite-mobDet-DSP-coco-320x320 alpha=0.400000 viz-threshold=0.600000 top-N=5 ! queue ! mosaic_0. \
+                                tiovxmosaic target=1 name=mosaic_0 \
+                                sink_0::startx=\"<320>\"  sink_0::starty=\"<180>\"  sink_0::widths=\"<1280>\"   sink_0::heights=\"<720>\"  \
+                                ! video/x-raw,format=NV12, width=1920, height=1080 ! queue ! tiperfoverlay main-title=null title=\"Object Detection\" ! ";
+
+static string ss_pipeline = "   multifilesrc location=/opt/edgeai-test-data/videos/video_0000_h264.h264 loop=true caps=\"video/x-h264, width=1280, height=720\" ! h264parse ! v4l2h264dec ! video/x-raw,format=NV12 ! \
+                                tiovxmultiscaler name=split_01 \
+                                split_01. ! queue ! video/x-raw, width=512, height=512 ! tiovxdlpreproc data-type=3 channel-order=0 tensor-format=rgb out-pool-size=4 ! application/x-tensor-tiovx ! tidlinferer model=/opt/model_zoo/ONR-SS-8610-deeplabv3lite-mobv2-ade20k32-512x512 ! post_0.tensor \
+                                split_01. ! queue ! video/x-raw, width=1280, height=720 ! post_0.sink \
+                                tidlpostproc name=post_0 model=/opt/model_zoo/ONR-SS-8610-deeplabv3lite-mobv2-ade20k32-512x512 alpha=0.400000 viz-threshold=0.500000 top-N=5 ! queue ! mosaic_0. \
+                                tiovxmosaic target=1 name=mosaic_0 \
+                                sink_0::startx=\"<320>\"  sink_0::starty=\"<180>\"  sink_0::widths=\"<1280>\"   sink_0::heights=\"<720>\"  \
+                                ! video/x-raw,format=NV12, width=1920, height=1080 ! queue ! tiperfoverlay main-title=null title=\"Semantic Segmentation \" ! ";
 
 class ButtonsClicked : public QObject {
     Q_OBJECT
@@ -81,17 +103,16 @@ public:
 
         cout << "c++ active button value = " << activeButton << endl;
         if (button == 1) {
-            // pipeline << "gst-pipeline: videotestsrc pattern=18 ! videoconvert ! videoscale ! capsfilter caps=\"video/x-raw, width=" << width << ", height=" << height << "\" ! qtvideosink";
-            pipeline = "gst-pipeline: " + od_pipeline;
+            pipeline = "gst-pipeline: " + cl_pipeline;
             addSink(pipeline, x, y, width, height);
         } else if (button == 2) {           
-            // pipeline << "gst-pipeline: videotestsrc pattern=18 ! videoconvert ! videoscale ! capsfilter caps=\"video/x-raw, width=" << width << ", height=" << height << "\" ! qtvideosink";
             pipeline = "gst-pipeline: " + od_pipeline;
             addSink(pipeline, x, y, width, height);
+            pipeline += " sync=false";
         } else if (button == 3) {
-            // pipeline << "gst-pipeline: videotestsrc pattern=18 ! videoconvert ! videoscale ! capsfilter caps=\"video/x-raw, width=" << width << ", height=" << height << "\" ! qtvideosink";
-            pipeline = "gst-pipeline: " + od_pipeline;
+            pipeline = "gst-pipeline: " + ss_pipeline;
             addSink(pipeline, x, y, width, height);
+            pipeline += " sync=false";
         } else if (button >= 4) {
             printf("WARNING: Button functionality not implemented\n");
         } else {
