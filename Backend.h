42b2c0adea999a64221c08180af3b803b64ffb43
Abhay Chirania <a-chirania@ti.com>
2023-02-23 16:09:07 +0530

    Backend: Update video and model for object detection
    Signed-off-by: Abhay Chirania <a-chirania@ti.com>

--- Backend.h
+++ Backend.h
@@ -16,16 +16,16 @@ static string cl_pipeline = "   multifilesrc location=/opt/edgeai-test-data/vide
                                 sink_0::startx=\"<320>\"  sink_0::starty=\"<180>\"  sink_0::widths=\"<1280>\"   sink_0::heights=\"<720>\"  \
                                 ! video/x-raw,format=NV12, width=1920, height=1080 ! queue ! tiperfoverlay main-title=null title=\"Image Classification\" ! ";
 
-static string od_pipeline = "   multifilesrc location=/opt/edgeai-test-data/videos/video_0000_h264.h264 loop=true caps=\"video/x-h264, width=1280, height=720\" ! h264parse ! v4l2h264dec ! video/x-raw,format=NV12 ! \
+static string od_pipeline = "   multifilesrc location=/opt/edgeai-test-data/videos/video_0003_h264.h264 loop=true caps=\"video/x-h264, width=1280, height=720\" ! h264parse ! v4l2h264dec ! video/x-raw,format=NV12 ! \
                                 tiovxmultiscaler name=split_01 \
-                                split_01. ! queue ! video/x-raw, width=320, height=320 ! tiovxdlpreproc data-type=3 channel-order=1 tensor-format=rgb out-pool-size=4 ! application/x-tensor-tiovx ! tidlinferer model=/opt/model_zoo/TFL-OD-2020-ssdLite-mobDet-DSP-coco-320x320 ! post_0.tensor \
+                                split_01. ! queue ! video/x-raw, width=416, height=416 ! tiovxdlpreproc data-type=3 channel-order=0 tensor-format=bgr out-pool-size=4 ! application/x-tensor-tiovx ! tidlinferer target=1 model=/opt/model_zoo/ONR-OD-8200-yolox-nano-lite-mmdet-coco-416x416 ! post_0.tensor \
                                 split_01. ! queue ! video/x-raw, width=1280, height=720 ! post_0.sink \
-                                tidlpostproc name=post_0 model=/opt/model_zoo/TFL-OD-2020-ssdLite-mobDet-DSP-coco-320x320 alpha=0.400000 viz-threshold=0.600000 top-N=5 ! queue ! mosaic_0. \
+                                tidlpostproc name=post_0 model=/opt/model_zoo/ONR-OD-8200-yolox-nano-lite-mmdet-coco-416x416 alpha=0.400000 viz-threshold=0.500000 top-N=5 ! queue ! mosaic_0. \
                                 tiovxmosaic target=1 name=mosaic_0 \
                                 sink_0::startx=\"<320>\"  sink_0::starty=\"<180>\"  sink_0::widths=\"<1280>\"   sink_0::heights=\"<720>\"  \
                                 ! video/x-raw,format=NV12, width=1920, height=1080 ! queue ! tiperfoverlay main-title=null title=\"Object Detection\" ! ";
 
-static string ss_pipeline = "   multifilesrc location=/opt/edgeai-test-data/videos/video_0000_h264.h264 loop=true caps=\"video/x-h264, width=1280, height=720\" ! h264parse ! v4l2h264dec ! video/x-raw,format=NV12 ! \
+static string ss_pipeline = "   multifilesrc location=/opt/edgeai-test-data/videos/video_0003_h264.h264 loop=true caps=\"video/x-h264, width=1280, height=720\" ! h264parse ! v4l2h264dec ! video/x-raw,format=NV12 ! \
                                 tiovxmultiscaler name=split_01 \
                                 split_01. ! queue ! video/x-raw, width=512, height=512 ! tiovxdlpreproc data-type=3 channel-order=0 tensor-format=rgb out-pool-size=4 ! application/x-tensor-tiovx ! tidlinferer model=/opt/model_zoo/ONR-SS-8610-deeplabv3lite-mobv2-ade20k32-512x512 ! post_0.tensor \
                                 split_01. ! queue ! video/x-raw, width=1280, height=720 ! post_0.sink \
