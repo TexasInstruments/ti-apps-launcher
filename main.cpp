6fa2cae9def68bea713f0720f30dac60c9a02a83
Subhrajeet <l-biswal@ti.com>
2023-06-16 13:52:25 +0530

    added all qml file in app dir
    Signed-off-by: Subhrajeet <l-biswal@ti.com>

--- main.cpp
+++ main.cpp
@@ -115,7 +115,7 @@ int main(int argc, char *argv[]) {
     engine.rootContext()->setContextProperty("cameraNamesList", &cameraNamesList);
     engine.rootContext()->setContextProperty("modelNamesList", &modelNamesList);
 
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/apps/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
