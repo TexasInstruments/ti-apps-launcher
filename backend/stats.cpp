58fcefac7dad952331b34e2f3c720aa44e080025
Subhrajeet <l-biswal@ti.com>
2023-06-28 13:49:00 +0530

    remove ddr bw functions
    Signed-off-by: Subhrajeet <l-biswal@ti.com>

--- backend/stats.cpp
+++ backend/stats.cpp
@@ -26,18 +26,18 @@ QString stats::getcpuload() {
     return res;
 }
 
-uint32_t stats::getddrtotalbw() {
-    perf_stats_ddr_stats_t *ddrload;
-    ddrload=perfStatsDdrStatsGet();
-    perfStatsResetDdrLoadCalcAll();
-    uint32_t res = ddrload->total_available_bw;
-    return res;
-}
-
-uint32_t stats::getddrload() {
-    perf_stats_ddr_stats_t *ddrload;
-    ddrload=perfStatsDdrStatsGet();
-    uint32_t r= ddrload->read_bw_avg+ddrload->write_bw_avg;
-    perfStatsResetDdrLoadCalcAll();
-    return r;
-}
+//uint32_t stats::getddrtotalbw() {
+//    perf_stats_ddr_stats_t *ddrload;
+//    ddrload=perfStatsDdrStatsGet();
+//    perfStatsResetDdrLoadCalcAll();
+//    uint32_t res = ddrload->total_available_bw;
+//    return res;
+//}
+//
+//uint32_t stats::getddrload() {
+//    perf_stats_ddr_stats_t *ddrload;
+//    ddrload=perfStatsDdrStatsGet();
+//    uint32_t r= ddrload->read_bw_avg+ddrload->write_bw_avg;
+//    perfStatsResetDdrLoadCalcAll();
+//    return r;
+//}
