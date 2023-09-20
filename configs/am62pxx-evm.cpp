07b0792f61d02544030e490a6a5972f134975628
Sai Sree Kartheek Adivi <s-adivi@ti.com>
2023-09-20 19:03:42 +0530

    Update AM62P config to include all apps as AM62 except 3d demo, arm analytics and live camera
    Signed-off-by: Sai Sree Kartheek Adivi <s-adivi@ti.com>

--- configs/am62pxx-evm.cpp
+++ configs/am62pxx-evm.cpp
@@ -2,16 +2,22 @@
 
 #include <iostream>
 #include "backend/includes/common.h"
+#include "backend/includes/run_cmd.h"
 #include "backend/includes/settings.h"
 #include "backend/includes/gpu_performance.h"
 #include "backend/includes/benchmarks.h"
 
 #define PLATFORM "am62pxx-evm"
 using namespace std;
-int include_apps_count = 3;
+int include_apps_count = 6;
 QString platform = "am62pxx-evm";
 
 app_info include_apps[] = {
+    {
+        .qml_source = "industrial_control.qml",
+        .name = "Industrial HMI",
+        .icon_source = "qrc:/images/hmi.png"
+    },
     {
         .qml_source = "benchmarks.qml",
         .name = "Benchmarks",
@@ -21,6 +27,16 @@ app_info include_apps[] = {
         .qml_source = "gpu_performance.qml",
         .name = "GPU Performance",
         .icon_source = "qrc:/images/gpu_performance.png"
+    },
+    {
+        .qml_source = "seva_store.qml",
+        .name = "Seva Store",
+        .icon_source = "qrc:/images/seva_store.png"
+    },
+    {
+        .qml_source = "firefox_browser.qml",
+        .name = "Firefox",
+        .icon_source = "qrc:/images/firefox.png"
     },
     {
         .qml_source = "settings.qml",
@@ -33,8 +49,14 @@ Settings settings;
 Benchmarks benchmarks;
 Gpu_performance gpuperformance;
 
+QString seva_command = QString::fromStdString("seva-launcher-aarch64");
+RunCmd *seva_store = new RunCmd(seva_command);
+RunCmd *firefox_browser = new RunCmd(QStringLiteral("docker run -v /run/user/1000/:/tmp/ -i --env http_proxy --env https_proxy --env no_proxy --env XDG_RUNTIME_DIR=/tmp/ --env WAYLAND_DISPLAY=wayland-1 -u user ghcr.io/texasinstruments/seva-browser:v1.0.0 https://www.ti.com/microcontrollers-mcus-processors/arm-based-processors/overview.html"));
+
 void platform_setup(QQmlApplicationEngine *engine) {
-    std::cout << "Running Platform Setup of AM62Px EVM!" << endl;
+    std::cout << "Running Platform Setup of AM62P!" << endl;
+    engine->rootContext()->setContextProperty("seva_store", seva_store);
+    engine->rootContext()->setContextProperty("firefox_browser", firefox_browser);
     engine->rootContext()->setContextProperty("settings", &settings);
     engine->rootContext()->setContextProperty("benchmarks", &benchmarks);
     engine->rootContext()->setContextProperty("gpuperformance", &gpuperformance);
