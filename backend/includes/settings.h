TexasInstruments/ti-apps-launcher

commit f0bf9eb2943832dd5c86f2c178fb3c8a4e389964

Do not write to users /etc/environment file
We can set our local application environment and the environment
for child programs. Changing the users system configuration is
not a good practice. It also simply appends to the file causing
it to grow endlessly. Remove this.

While here, switch to a more updated Qt code style.

Signed-off-by: Andrew Davis <afd@ti.com>

backend/includes/settings.h
@@ -1,20 +1,15 @@
-#include <unordered_map>
 #include <QString>
 #include <QObject>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
-#include <QProcess>
-
-using namespace std;
-extern unordered_map<string, string> proxy_list;
 
 class Settings : public QObject {
     Q_OBJECT
 
 public:
-    QString _https_proxy;
-    QString _no_proxy;
+    QString https_proxy;
+    QString no_proxy;
     Q_INVOKABLE void set_proxy(QString https_proxy, QString no_proxy);
 
+private:
+    void set_environment_variables();
+    void apply_docker_proxy();
 };
-

backend/settings.cpp
@@ -1,90 +1,56 @@
 #include "includes/settings.h"
-#include <iostream>
+
 #include <filesystem>
 #include <stdlib.h>
-#include <unordered_map>
 #include <sys/stat.h>
 #include <sys/types.h>
 
-using namespace std;
+#include <QDebug>
 
-unordered_map<string, string> proxy_list;
-
-void set_environment_variables() {
-    // Setting up Environment Variables
-    string command = "echo https_proxy=" + proxy_list["https_proxy"] + " | tee -a /etc/environment";
-    system(command.c_str());
-
-    command = "echo HTTPS_PROXY=" + proxy_list["https_proxy"] + " | tee -a /etc/environment";
-    system(command.c_str());
-
-    command = "echo HTTP_PROXY=" + proxy_list["https_proxy"] + " | tee -a /etc/environment";
-    system(command.c_str());
-
-    command = "echo http_proxy=" + proxy_list["https_proxy"] + " | tee -a /etc/environment";
-    system(command.c_str());
-
-    command = "echo ftp_proxy=" + proxy_list["https_proxy"] + " | tee -a /etc/environment";
-    system(command.c_str());
-
-    command = "echo FTP_PROXY=" + proxy_list["https_proxy"] + " | tee -a /etc/environment";
-    system(command.c_str());
-
-    command = "echo no_proxy=" + proxy_list["no_proxy"] + " | tee -a /etc/environment";
-    system(command.c_str());
-
-    setenv("https_proxy",proxy_list["https_proxy"].c_str(),1);
-    setenv("http_proxy",proxy_list["https_proxy"].c_str(),1);
-    setenv("ftp_proxy",proxy_list["https_proxy"].c_str(),1);
-    setenv("no_proxy",proxy_list["no_proxy"].c_str(),1);
-    setenv("HTTPS_PROXY",proxy_list["https_proxy"].c_str(),1);
-    setenv("HTTP_PROXY",proxy_list["https_proxy"].c_str(),1);
-    setenv("FTP_PROXY",proxy_list["https_proxy"].c_str(),1);
+void Settings::set_environment_variables() {
+    qputenv("https_proxy", https_proxy.toUtf8());
+    qputenv("http_proxy", https_proxy.toUtf8());
+    qputenv("ftp_proxy", https_proxy.toUtf8());
+    qputenv("no_proxy", no_proxy.toUtf8());
+    qputenv("HTTPS_PROXY", https_proxy.toUtf8());
+    qputenv("HTTP_PROXY", https_proxy.toUtf8());
+    qputenv("FTP_PROXY", https_proxy.toUtf8());
 }
 
-
-void apply_docker_proxy() {
+void Settings::apply_docker_proxy() {
     // Create /etc/systemd/system/docker.service.d directory structure
     if(system("mkdir -p /etc/systemd/system/docker.service.d") == -1){
-        cout << "Failed to create directory structure for docker proxies" << endl;
-        exit(0);
+        qCritical() << "Failed to create directory structure for docker proxies";
+        return;
     }
 
     // Write the docker proxy setting
-    string apply_systemd_proxy = "echo [Service] | tee /etc/systemd/system/docker.service.d/http-proxy.conf";
-    system(apply_systemd_proxy.c_str());
+    QString apply_systemd_proxy = "echo [Service] | tee /etc/systemd/system/docker.service.d/http-proxy.conf";
+    system(qPrintable(apply_systemd_proxy));
 
-    apply_systemd_proxy = "echo Environment=HTTPS_PROXY=" + proxy_list["https_proxy"] + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
-    system(apply_systemd_proxy.c_str());
+    apply_systemd_proxy = "echo Environment=HTTPS_PROXY=" + https_proxy + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
+    system(qPrintable(apply_systemd_proxy));
 
-    apply_systemd_proxy = "echo Environment=HTTP_PROXY=" + proxy_list["https_proxy"] + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
-    system(apply_systemd_proxy.c_str());
+    apply_systemd_proxy = "echo Environment=HTTP_PROXY=" + https_proxy + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
+    system(qPrintable(apply_systemd_proxy));
 
-    apply_systemd_proxy = "echo Environment=NO_PROXY=" + proxy_list["no_proxy"] + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
-    system(apply_systemd_proxy.c_str());
+    apply_systemd_proxy = "echo Environment=NO_PROXY=" + no_proxy + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
+    system(qPrintable(apply_systemd_proxy));
 
     // Flush changes, Re-load Daemon and Re-Start Docker
-    string str = "systemctl daemon-reload";
-    const char *command = str.c_str();
-    system(command);
-
-    str = "systemctl restart docker";
-    command = str.c_str();
-    system(command);
+    system("systemctl daemon-reload");
+    system("systemctl restart docker");
 }
 
 void Settings::set_proxy(QString https_proxy, QString no_proxy) {
-    Settings::_https_proxy=https_proxy;
-    Settings::_no_proxy=no_proxy;
+    qInfo() << "Setting https_proxy to " << https_proxy;
+    qInfo() << "Setting no_proxy to " << no_proxy;
 
-    cout << "Setting https_proxy to " << https_proxy.toStdString() << endl;
-    cout << "Setting no_proxy to " << no_proxy.toStdString() << endl;
-
-    proxy_list["https_proxy"] = https_proxy.toStdString();
-    proxy_list["no_proxy"] = no_proxy.toStdString();
+    this->https_proxy = https_proxy;
+    this->no_proxy = no_proxy;
 
     apply_docker_proxy();
     set_environment_variables();
 
-    cout << "Applied Proxy Settings!" << endl;
+    qInfo() << "Applied Proxy Settings!";
 }

backend/includes/run_cmd.h
@@ -1,9 +1,6 @@
-#include <QString>
 #include <QObject>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
+#include <QString>
 #include <QProcess>
-#include <unordered_map>
 
 class RunCmd : public QObject {
     Q_OBJECT
@@ -14,17 +11,12 @@ private:
 
     QString _command;
     QProcess process;
-    QProcessEnvironment env;
 
 public:
-    Q_PROPERTY(QString button READ button NOTIFY button_changed);
-    Q_PROPERTY(QString status_msg READ status_msg NOTIFY status_msg_changed);
+    Q_PROPERTY(QString button MEMBER _button NOTIFY button_changed);
+    Q_PROPERTY(QString status_msg MEMBER _status_msg NOTIFY status_msg_changed);
 
     RunCmd(QString command);
-
-    Q_INVOKABLE QString button();
-
-    QString status_msg();
 
     Q_INVOKABLE void launch_or_stop();
     Q_INVOKABLE void run(QString command);

backend/run_cmd.cpp
@@ -1,40 +1,30 @@
 #include "includes/run_cmd.h"
-#include <iostream>
-#include <unordered_map>
+#include "includes/settings.h"
 
-using namespace std;
+#include <QDebug>
 
-extern unordered_map<string, string> proxy_list;
+extern Settings settings;
 
 void RunCmd::onStateChanged(const QProcess::ProcessState &new_state) {
     if( process.state() == QProcess::NotRunning ) {
-        cout << "Exited " << _command.toStdString() << endl;
+        qInfo() << "Exited " << _command;
         _button = "Launch";
         _status_msg = "Click 'Launch' to run.";
     } else if ( process.state() == QProcess::Running ) {
-        cout << "started " << _command.toStdString() << endl;
+        qInfo() << "Started " << _command;
         _button = "Stop";
         _status_msg = "Click 'Stop' to kill.";
     } else {
-        cout << "starting " << _command.toStdString() << endl;
+        qInfo() << "Starting " << _command;
     }
     Q_EMIT button_changed();
     Q_EMIT status_msg_changed();
-}
-
-QString RunCmd::button() {
-    return _button;
-}
-
-QString RunCmd::status_msg() {
-    return _status_msg;
 }
 
 RunCmd::RunCmd(QString command) {
     _command = command;
     _status_msg = "Click 'Launch' to run.";
     _button = "Launch";
-    env = QProcessEnvironment::systemEnvironment();
     QObject::connect(&process, &QProcess::stateChanged, this, &RunCmd::onStateChanged);
 }
 
@@ -42,20 +32,21 @@ void RunCmd::launch_or_stop() {
     QByteArray output_bytes;
     QString output_string;
     if( process.state() == QProcess::NotRunning ) {
+        QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
         env.insert("XDG_RUNTIME_DIR", "/run/user/1000");
         env.insert("WAYLAND_DISPLAY", "wayland-1");
         env.insert("QT_QPA_PLATFORM", "wayland");
-        if(proxy_list["https_proxy"].size()) {
-            env.insert("http_proxy",proxy_list["https_proxy"].c_str());
-            env.insert("https_proxy",proxy_list["https_proxy"].c_str());
-            env.insert("ftp_proxy",proxy_list["https_proxy"].c_str());
-            env.insert("HTTP_PROXY",proxy_list["https_proxy"].c_str());
-            env.insert("HTTPS_PROXY",proxy_list["https_proxy"].c_str());
-            env.insert("FTP_PROXY",proxy_list["https_proxy"].c_str());
+        if( settings.https_proxy != "" ) {
+            env.insert("http_proxy", settings.https_proxy);
+            env.insert("https_proxy", settings.https_proxy);
+            env.insert("ftp_proxy", settings.https_proxy);
+            env.insert("HTTP_PROXY", settings.https_proxy);
+            env.insert("HTTPS_PROXY", settings.https_proxy);
+            env.insert("FTP_PROXY", settings.https_proxy);
         }
-        if(proxy_list["no_proxy"].size()) {
-            env.insert("no_proxy",proxy_list["no_proxy"].c_str());
-            env.insert("NO_PROXY",proxy_list["no_proxy"].c_str());
+        if( settings.no_proxy != "" ) {
+            env.insert("no_proxy", settings.no_proxy);
+            env.insert("NO_PROXY", settings.no_proxy);
         }
         process.setProcessEnvironment(env);
         process.start(_command);
@@ -70,20 +61,21 @@ void RunCmd::run(QString command) {
     _command = command;
 
     if( process.state() == QProcess::NotRunning ) {
+        QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
         env.insert("XDG_RUNTIME_DIR", "/run/user/1000");
         env.insert("WAYLAND_DISPLAY", "wayland-1");
         env.insert("QT_QPA_PLATFORM", "wayland");
-        if(proxy_list["https_proxy"].size()) {
-            env.insert("http_proxy",proxy_list["https_proxy"].c_str());
-            env.insert("https_proxy",proxy_list["https_proxy"].c_str());
-            env.insert("ftp_proxy",proxy_list["https_proxy"].c_str());
-            env.insert("HTTP_PROXY",proxy_list["https_proxy"].c_str());
-            env.insert("HTTPS_PROXY",proxy_list["https_proxy"].c_str());
-            env.insert("FTP_PROXY",proxy_list["https_proxy"].c_str());
+        if( settings.https_proxy != "" ) {
+            env.insert("http_proxy", settings.https_proxy);
+            env.insert("https_proxy", settings.https_proxy);
+            env.insert("ftp_proxy", settings.https_proxy);
+            env.insert("HTTP_PROXY", settings.https_proxy);
+            env.insert("HTTPS_PROXY", settings.https_proxy);
+            env.insert("FTP_PROXY", settings.https_proxy);
         }
-        if(proxy_list["no_proxy"].size()) {
-            env.insert("no_proxy",proxy_list["no_proxy"].c_str());
-            env.insert("NO_PROXY",proxy_list["no_proxy"].c_str());
+        if( settings.no_proxy != "" ) {
+            env.insert("no_proxy", settings.no_proxy);
+            env.insert("NO_PROXY", settings.no_proxy);
         }
         process.setProcessEnvironment(env);
         process.start(_command);

commit 27554d31e5b304819ee54d643f3df849ea8fc13b

backend: settings: fix seva-launcher not working due to proxy settings
Commit f0bf9eb removed the logic that applied the global proxy configuration
to systemd services. As a result the `seva‑launcher` service started without
the required proxy variables and failed when it tried to download `metadata.json`
from GitHub.

This patch fixes the bug introduced by commit f0bf9eb by applying the proxy
settings to seva-launcher service by creating a drop-in file for respective
systemd service.

Signed-off-by: Yogesh Hegde <y-hegde@ti.com>

backend/includes/settings.h
@@ -11,5 +11,5 @@ public:
 
 private:
     void set_environment_variables();
-    void apply_docker_proxy();
+    void install_systemd_proxy_dropin(const QString &service_name);
 };

backend/settings.cpp
@@ -17,29 +17,45 @@ void Settings::set_environment_variables() {
     qputenv("FTP_PROXY", https_proxy.toUtf8());
 }
 
-void Settings::apply_docker_proxy() {
-    // Create /etc/systemd/system/docker.service.d directory structure
-    if(system("mkdir -p /etc/systemd/system/docker.service.d") == -1){
-        qCritical() << "Failed to create directory structure for docker proxies";
+/**
+ * @brief Writes a systemd drop‑in file containing the current proxy env vars.
+ * 
+ * @param service_dropin_dir  Path to the service's ".service.d" directory,
+ *                          e.g. "/etc/systemd/system/docker.service.d".
+ */
+void Settings::install_systemd_proxy_dropin(const QString &service_name) {
+
+    /* Check if input is empty */
+    if(service_name.isEmpty()) {
+        return;
+    }
+    QString service_dropin_path = "/etc/systemd/system/" + service_name + ".service.d";
+    /* Create /etc/systemd/system/myservice.service.d directory structure */
+    QString mkdir_cmd = "mkdir -p /etc/systemd/system/" + service_dropin_path;
+    if(system(qPrintable(mkdir_cmd)) == -1){
+        qCritical() << "Failed to create directory structure for proxies at " << service_dropin_path;
         return;
     }
 
-    // Write the docker proxy setting
-    QString apply_systemd_proxy = "echo [Service] | tee /etc/systemd/system/docker.service.d/http-proxy.conf";
+    /* Write the proxy setting */
+    QString apply_systemd_proxy = "echo [Service] | tee " + service_dropin_path + "/http-proxy.conf";
     system(qPrintable(apply_systemd_proxy));
 
-    apply_systemd_proxy = "echo Environment=HTTPS_PROXY=" + https_proxy + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
+    apply_systemd_proxy = "echo Environment=HTTPS_PROXY=" + https_proxy + " | tee -a " + service_dropin_path + "/http-proxy.conf";
     system(qPrintable(apply_systemd_proxy));
 
-    apply_systemd_proxy = "echo Environment=HTTP_PROXY=" + https_proxy + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
+    apply_systemd_proxy = "echo Environment=HTTP_PROXY=" + https_proxy + " | tee -a " + service_dropin_path + "/http-proxy.conf";
     system(qPrintable(apply_systemd_proxy));
 
-    apply_systemd_proxy = "echo Environment=NO_PROXY=" + no_proxy + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
+    apply_systemd_proxy = "echo Environment=NO_PROXY=" + no_proxy + " | tee -a " + service_dropin_path + "/http-proxy.conf";
     system(qPrintable(apply_systemd_proxy));
 
-    // Flush changes, Re-load Daemon and Re-Start Docker
+    /* Flush changes, Re-load Daemon */
     system("systemctl daemon-reload");
-    system("systemctl restart docker");
+
+    /* Restart service */
+    QString systemd_restart_cmd = "systemctl restart " + service_name;
+    system(qPrintable(systemd_restart_cmd));
 }
 
 void Settings::set_proxy(QString https_proxy, QString no_proxy) {
@@ -49,7 +65,8 @@ void Settings::set_proxy(QString https_proxy, QString no_proxy) {
     this->https_proxy = https_proxy;
     this->no_proxy = no_proxy;
 
-    apply_docker_proxy();
+    install_systemd_proxy_dropin("docker");
+    install_systemd_proxy_dropin("seva-launcher");
     set_environment_variables();
 
     qInfo() << "Applied Proxy Settings!";