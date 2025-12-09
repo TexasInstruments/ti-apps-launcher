TexasInstruments/ti-apps-launcher

commit 27554d31e5b304819ee54d643f3df849ea8fc13b

backend: settings: fix seva-launcher not working due to proxy settings
Commit f0bf9eb removed the logic that applied the global proxy configuration
to systemd services. As a result the `seva‑launcher` service started without
the required proxy variables and failed when it tried to download `metadata.json`
from GitHub.

This patch fixes the bug introduced by commit f0bf9eb by applying the proxy
settings to seva-launcher service by creating a drop-in file for respective
systemd service.

Signed-off-by: Yogesh Hegde <y-hegde@ti.com>

backend/settings.cpp
@@ -17,29 +17,45 @@ void Settings::set_environment_variables() {
     qputenv("FTP_PROXY", https_proxy.toUtf8());
 }
 
-void Settings::apply_docker_proxy() {
-    // Create /etc/systemd/system/docker.service.d directory structure
-    if(system("mkdir -p /etc/systemd/system/docker.service.d") == -1){
-        qCritical() << "Failed to create directory structure for docker proxies";
+/**
+ * @brief Writes a systemd drop‑in file containing the current proxy env vars.
+ * 
+ * @param service_dropin_dir  Path to the service's ".service.d" directory,
+ *                          e.g. "/etc/systemd/system/docker.service.d".
+ */
+void Settings::install_systemd_proxy_dropin(const QString &service_name) {
+
+    /* Check if input is empty */
+    if(service_name.isEmpty()) {
+        return;
+    }
+    QString service_dropin_path = "/etc/systemd/system/" + service_name + ".service.d";
+    /* Create /etc/systemd/system/myservice.service.d directory structure */
+    QString mkdir_cmd = "mkdir -p /etc/systemd/system/" + service_dropin_path;
+    if(system(qPrintable(mkdir_cmd)) == -1){
+        qCritical() << "Failed to create directory structure for proxies at " << service_dropin_path;
         return;
     }
 
-    // Write the docker proxy setting
-    QString apply_systemd_proxy = "echo [Service] | tee /etc/systemd/system/docker.service.d/http-proxy.conf";
+    /* Write the proxy setting */
+    QString apply_systemd_proxy = "echo [Service] | tee " + service_dropin_path + "/http-proxy.conf";
     system(qPrintable(apply_systemd_proxy));
 
-    apply_systemd_proxy = "echo Environment=HTTPS_PROXY=" + https_proxy + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
+    apply_systemd_proxy = "echo Environment=HTTPS_PROXY=" + https_proxy + " | tee -a " + service_dropin_path + "/http-proxy.conf";
     system(qPrintable(apply_systemd_proxy));
 
-    apply_systemd_proxy = "echo Environment=HTTP_PROXY=" + https_proxy + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
+    apply_systemd_proxy = "echo Environment=HTTP_PROXY=" + https_proxy + " | tee -a " + service_dropin_path + "/http-proxy.conf";
     system(qPrintable(apply_systemd_proxy));
 
-    apply_systemd_proxy = "echo Environment=NO_PROXY=" + no_proxy + " | tee -a /etc/systemd/system/docker.service.d/http-proxy.conf";
+    apply_systemd_proxy = "echo Environment=NO_PROXY=" + no_proxy + " | tee -a " + service_dropin_path + "/http-proxy.conf";
     system(qPrintable(apply_systemd_proxy));
 
-    // Flush changes, Re-load Daemon and Re-Start Docker
+    /* Flush changes, Re-load Daemon */
     system("systemctl daemon-reload");
-    system("systemctl restart docker");
+
+    /* Restart service */
+    QString systemd_restart_cmd = "systemctl restart " + service_name;
+    system(qPrintable(systemd_restart_cmd));
 }
 
 void Settings::set_proxy(QString https_proxy, QString no_proxy) {
@@ -49,7 +65,8 @@ void Settings::set_proxy(QString https_proxy, QString no_proxy) {
     this->https_proxy = https_proxy;
     this->no_proxy = no_proxy;
 
-    apply_docker_proxy();
+    install_systemd_proxy_dropin("docker");
+    install_systemd_proxy_dropin("seva-launcher");
     set_environment_variables();
 
     qInfo() << "Applied Proxy Settings!";

backend/includes/settings.h
@@ -11,5 +11,5 @@ public:
 
 private:
     void set_environment_variables();
-    void apply_docker_proxy();
+    void install_systemd_proxy_dropin(const QString &service_name);
 };

commit f84753a9af458d3a0d8dd5419d51d6cbd814e85c

backend: settings: fix seva-launcher
Fixes the bug introduced by commit 27554d3 where mkdir command
is run in wrong folder.

Signed-off-by: Yogesh Hegde <y-hegde@ti.com>

backend/settings.cpp
@@ -31,7 +31,7 @@ void Settings::install_systemd_proxy_dropin(const QString &service_name) {
     }
     QString service_dropin_path = "/etc/systemd/system/" + service_name + ".service.d";
     /* Create /etc/systemd/system/myservice.service.d directory structure */
-    QString mkdir_cmd = "mkdir -p /etc/systemd/system/" + service_dropin_path;
+    QString mkdir_cmd = "mkdir -p " + service_dropin_path;
     if(system(qPrintable(mkdir_cmd)) == -1){
         qCritical() << "Failed to create directory structure for proxies at " << service_dropin_path;
         return;